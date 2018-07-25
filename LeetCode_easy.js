f35a342e0020f63b99d6b3cfa85b3941d987dd54
Charlie Thomas <cjthom03@gmail.com>
2018-07-25 11:29:51 -0400

    Start easy js algo - longest common prefix

---
+++ LeetCode_easy.js
@@ -0,0 +1,31 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+//
+// If there is no common prefix, return an empty string "".
+//
+// Example 1:
+//
+// Input: ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+//
+// Input: ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+// Note:
+//
+// All given inputs are in lowercase letters a-z.
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  //iterate over each word in the array
+  // check the letter at index i
+  // break out of the loop if there is no letter at index i
+  // store the letter from the first word
+  // if the letter is different in any word after the first, break out of the loop
+  // if the letters are all the same, store the letter in a result string
+//  return the result string
+
+};
