db47bf6e521cce182a1164cb063d227ea75f90ba
Charlie Thomas <cjthom03@gmail.com>
2018-07-25 11:41:31 -0400

    complete brute force longest common prefix

--- LeetCode_easy.js
+++ LeetCode_easy.js
@@ -26,6 +26,26 @@ var longestCommonPrefix = function(strs) {
   // store the letter from the first word
   // if the letter is different in any word after the first, break out of the loop
   // if the letters are all the same, store the letter in a result string
-//  return the result string
+  //  return the result string
+  let result = "";
+
+  if(!strs.length) { return result; }
+
+  for (let i = 0; i < strs[0].length; i++) {
+    let letter = strs[0][i];
+    for (let j = 1; j < strs.length; j++) {
+      if (i >= strs[j].length || strs[j][i] !== letter) {
+        return result;
+      }
+    }
+    result += letter;
+  }
+
+
+return result;
+
+
+  return result;
+
 
 };
