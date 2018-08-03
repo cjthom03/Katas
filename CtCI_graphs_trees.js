10f2b09bf4ba9ad7ac852c0db948d3f105c67bd7
Charlie Thomas <cjthom03@gmail.com>
2018-08-03 09:52:21 -0700

    Add file for js answers to CtCI graphs and trees

---
+++ CtCI_graphs_trees.js
@@ -0,0 +1,10 @@
+// 4.2 Minimal Tree
+// Given a sorted (ascending order) array with unique integer elements,
+// write an algorithm to build a binary search tree with minimal height
+
+
+
+
+
+// #======================================================================
+// #======================================================================
