b351b0271333870dcd79ee424b179abcfb0b9a17
Charlie Thomas <cjthom03@gmail.com>
2018-08-03 10:34:02 -0700

    Complete CtCI 4.2 in graphs and trees

--- CtCI_graphs_trees.js
+++ CtCI_graphs_trees.js
@@ -2,8 +2,25 @@
 // Given a sorted (ascending order) array with unique integer elements,
 // write an algorithm to build a binary search tree with minimal height
 
+function BSTNode(val) {
+  this.val = val;
+  this.left = null;
+  this.right = null;
+}
 
+function minimalBST(array) {
+  if(!array.length) { return null; }
+  const midpoint = Math.floor(array.length / 2);
+  const rootNode = new BSTNode(array[midpoint]);
 
+  let left = array.slice(0, midpoint);
+  let right = array.slice(midpoint + 1);
+
+  rootNode.left = minimalBST(left);
+  rootNode.right = minimalBST(right);
+
+  return rootNode;
+}
 
 
 // #======================================================================
