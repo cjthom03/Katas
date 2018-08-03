96537ca9b39453d313d7c3a21fc843fea051e0f9
Charlie Thomas <cjthom03@gmail.com>
2018-08-03 09:49:31 -0700

    Complete CtCI 4.1 graphs and trees

---
+++ CtCI_graphs_trees.rb
@@ -0,0 +1,65 @@
+# 4.1 - Route between nodes
+# Given a directed graph, design an algorithm to find
+# out whether there is a route between two nodes
+
+class Node
+  attr_accessor :edges
+
+  def initialize(value)
+    @value = value
+    @edges = []
+  end
+end
+
+class Edge
+  attr_reader :fromNode, :toNode
+  def initialize(fromNode, toNode)
+    @fromNode = fromNode
+    @toNode = toNode
+    fromNode.edges.push(self)
+  end
+end
+
+def route?(startNode, endNode)
+  visited = Hash.new
+  queue = Queue.new
+  queue.push(startNode)
+
+  until queue.empty?
+    currentNode = queue.pop
+    return true if currentNode == endNode
+
+    currentNode.edges.each do |edge|
+      child = edge.toNode
+      unless visited[child]
+        queue.push(child)
+        visited[child] = true
+      end
+    end
+  end
+
+  false
+end
+
+# test case
+
+nodeA = Node.new(:A) #Start Node
+nodeB = Node.new(:B) #End Node
+nodeC = Node.new(:C)
+nodeD = Node.new(:D)
+nodeE = Node.new(:E)
+nodeF = Node.new(:F)
+nodeG = Node.new(:G)
+
+Edge.new(nodeA, nodeF)
+Edge.new(nodeF, nodeG)
+Edge.new(nodeG, nodeF)
+Edge.new(nodeA, nodeC)
+Edge.new(nodeC, nodeD)
+Edge.new(nodeD, nodeB)
+Edge.new(nodeB, nodeD)
+Edge.new(nodeB, nodeE)
+
+
+#======================================================================
+#======================================================================
