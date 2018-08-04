1a588aef6e2faa6e0f2cc7ec86be6b6b1953f60b
Charlie Thomas <cjthom03@gmail.com>
2018-08-04 10:16:32 -0700

    Start 4.3 in CtCI graphs and trees

--- CtCI_graphs_trees.js
+++ CtCI_graphs_trees.js
@@ -25,3 +25,12 @@ function minimalBST(array) {
 
 // #======================================================================
 // #======================================================================
+//
+// 4.3 List of Depths
+// Given a binary tree, design an algorithm which creates a linked list of all
+// the nodes at each depth (e.g. if you have a tree with depth D, you'll have
+// D linked lists)
+
+
+// #======================================================================
+// #======================================================================
